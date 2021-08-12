dcaef59aaa4ebf41049f77a16a8125e525f6a313
mohammad mirani <mohammadmirani@gmail.com>
2021-08-12 23:51:12 +0430

    edit author

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Load = require("./public/main");
 const logger = new Load();
-console.log(typeof logger);
+// console.log(typeof logger);
 
 const http = require("http");
 const server = http.createServer((req, res) => {});
