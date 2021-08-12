54cdbd7beab290b2b4bc52642efa4a498fd2071b
Mohammad Mirani <49958457+MohammadMirani@users.noreply.github.com>
2021-08-13 00:33:32 +0430

    update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const Load = require("./public/main");
 const logger = new Load();
 console.log(typeof logger);
 
-// const http = require("http");
-// const server = http.createServer((req, res) => {});
-// server.listen(3000, () => {});
+const http = require("http");
+const server = http.createServer((req, res) => {});
+server.listen(3000, () => {});
