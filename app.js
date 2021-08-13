9b9ad5af65cc2577ca891024f6a3a972d66e6354
afruzmirani <afruzmirani@gmail.com>
2021-08-13 12:37:07 +0430

    i want

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Load = require("./public/main");
 const logger = new Load();
 console.log(typeof logger);
 console.log("Learning Fork By AfruzMirani");
+console.log("i want to  ...");
 
 const http = require("http");
 const server = http.createServer((req, res) => {});
