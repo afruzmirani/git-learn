f5a925469c692c1751aee18675a2aef9733de25f
mohammad mirani <mohammadmirani@gmail.com>
2021-08-13 12:27:15 +0430

    changes 1

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const Load = require("./public/main");
 const logger = new Load();
 console.log(typeof logger);
 console.log("Learning Fork By AfruzMirani");
-console.log("i want to ...");
 
 const http = require("http");
 const server = http.createServer((req, res) => {});
